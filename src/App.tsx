f9e3e2fba14e3cbceaba630f60038decda8738a1
charilaouc <charilaou.c@gmail.com>
2022-09-16 18:10:07 +0000

    Application bootstrapping and hasura dataprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Refine } from '@pankod/refine-core'; '@pankod/refine-core';
+import routerProvider from "@pankod/refine-react-router-v6";
+import { ErrorComponent, Layout, notificationProvider, ReadyPage } from '@pankod/refine-antd';
+import dataProvider, { GraphQLClient, graphqlWS } from '@pankod/refine-hasura';
 
-function App() {
+const WS_URL = "ws://smooth-kingfish-80.hasura.app/v1/graphql";
+
+const gqlWebSocketClient = graphqlWS.createClient({
+  url: WS_URL,
+  },
+);
+
+
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Refine
+    routerProvider={routerProvider}
+    dataProvider={gqlWebSocketClient}
+    Layout={Layout}
+    ReadyPage={ReadyPage}
+    notificationProvider={notificationProvider}
+    catchAll={<ErrorComponent />}
+    />
   );
 }
 
